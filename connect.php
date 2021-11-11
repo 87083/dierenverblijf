40964121f6832cdc56e42785c5c5e0998c25f084
87083@glr.nl <87083@glr.nl>
2021-11-11 11:56:32 +0100

    a'

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+$db_host = "localhost";
+$db_username = "85748_dieren";
+$db_password = "Pocketoli";
+$db_database = "dierenverblijf";
+
+try{
+    $database = new PDO("mysql:host={$db_host};dbname={$db_database}", $db_username, $db_password);
+    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connectie gelukt" . "<br/>";
+    // $database = null;
+} catch (PDOException $exception){
+    // echo "connectie mislukt";
+    print "Error!: " . $e->getMessage() . "<br/>";
+    // die();
+}
--- loginCheck.php
+++ loginCheck.php
@@ -1,8 +1,26 @@
 <?php
-  if(isset($_POST["submit"])){
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+  try{
+    require_once "connect.php";
 
-    echo "form verstuurd";
   }
+  catch(Exception $e){
+    $error = $e->getMessage();
+  }
+
+  if(isset($_POST["submit"])){
+  $stmt = $database->prepare("SELECT * FROM dierenverblijf WHERE username = ?");
+  $stmt->execute([$_POST['username']]);
+  $user = $stmt->fetch();
+
+
+  if ($user)
+  {
+    $_SESSION["username"] = $username;
+    echo "<h1><center> Login successful </center></h1>";  
+  } else {
+    echo "<h1> Login failed. Foute username of wachtwoord.</h1>";  
+  }
+} else {
+  eader("location:index.html");
+}
 ?>
